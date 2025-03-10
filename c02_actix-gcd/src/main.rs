048de106ba81edf039e5d690885d8b6d668bbf35
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2025-03-11 06:57:41 +0900

    Add http server for GCD

---
+++ c02_actix-gcd/src/main.rs
@@ -0,0 +1,28 @@
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let server = HttpServer::new(|| App::new().service(get_index));
+
+    println!("Serving on http://127.0.0.1:3000");
+
+    server
+        .bind("127.0.0.1:3000")
+        .expect("error binding server to address")
+        .run()
+        .await
+}
+
+#[get("/")]
+async fn get_index() -> impl Responder {
+    HttpResponse::Ok().content_type("text/html").body(
+        r#"
+            <title>GCD Calculator</title>
+            <form action="/gcd" method="post">
+            <input type="text" name="n" />
+            <input type="text" name="m" />
+            <button type="submit">Compute GCD</button>
+            </form>
+        "#,
+    )
+}
