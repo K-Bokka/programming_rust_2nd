3ee13f345464f7362cb46c4b8817440632a18445
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2025-03-12 07:30:16 +0900

    Change to use workspace

--- c02_actix-gcd/src/main.rs
+++ c02_actix-gcd/src/main.rs
@@ -1,5 +1,6 @@
 use actix_web::{App, HttpResponse, HttpServer, Responder, get, post, web};
 use serde::Deserialize;
+use c02_hello::gcd;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -49,17 +50,4 @@ async fn post_gcd(form: web::Form<GcdParameters>) -> impl Responder {
     );
 
     HttpResponse::Ok().content_type("text/html").body(response)
-}
-
-fn gcd(mut n: u64, mut m: u64) -> u64 {
-    assert!(n != 0 && m != 0);
-    while m != 0 {
-        if m < n {
-            let t = m;
-            m = n;
-            n = t;
-        }
-        m = m % n;
-    }
-    n
 }
--- c02_hello/src/main.rs
+++ c02_hello/src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::str::FromStr;
+use c02_hello::gcd;
 
 fn main() {
     // println!("Hello, world!");
@@ -20,23 +21,4 @@ fn main() {
         d = gcd(d, *m);
     }
     println!("The greatest common divisor of {:?} is {}", numbers, d);
-}
-
-fn gcd(mut n: u64, mut m: u64) -> u64 {
-    assert!(n != 0 && m != 0);
-    while m != 0 {
-        if m < n {
-            let t = m;
-            m = n;
-            n = t;
-        }
-        m = m % n;
-    }
-    n
-}
-
-#[test]
-fn test_gcd() {
-    assert_eq!(gcd(14, 15), 1);
-    assert_eq!(gcd(2 * 3 * 5 * 11 * 17, 3 * 11 * 19), 3 * 11);
 }
