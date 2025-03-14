e3a364d71579e8ca23256b94b6b1dd257482b89d
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2025-03-14 09:55:14 +0900

    Add functions

--- c02_mandelbrot/src/main.rs
+++ c02_mandelbrot/src/main.rs
@@ -1,4 +1,5 @@
 use num::Complex;
+use std::str::FromStr;
 
 fn main() {
     println!("Hello, world!");
@@ -22,4 +23,46 @@ fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
         z = z * z + c;
     }
     None
+}
+
+#[allow(dead_code)]
+fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
+    match s.find(separator) {
+        None => None,
+        Some(index) => match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
+            (Ok(l), Ok(r)) => Some((l, r)),
+            _ => None,
+        },
+    }
+}
+
+#[test]
+fn test_parse_pair() {
+    assert_eq!(parse_pair::<i32>("", ','), None);
+    assert_eq!(parse_pair::<i32>("10", ','), None);
+    assert_eq!(parse_pair::<i32>(",10", ','), None);
+    assert_eq!(parse_pair::<i32>("10,20", ','), Some((10, 20)));
+    assert_eq!(parse_pair::<i32>("10,20xy", ','), None);
+    assert_eq!(parse_pair::<f64>("0.5x", 'x'), None);
+    assert_eq!(parse_pair::<f64>("0.5x1.5", 'x'), Some((0.5, 1.5)));
+}
+
+#[allow(dead_code)]
+fn parse_complex(s: &str) -> Option<Complex<f64>> {
+    match parse_pair::<f64>(s, ',') {
+        None => None,
+        Some((re, im)) => Some(Complex { re, im }),
+    }
+}
+
+#[test]
+fn test_parse_complex() {
+    assert_eq!(
+        parse_complex("1.25,-0.0625"),
+        Some(Complex {
+            re: 1.25,
+            im: -0.0625
+        })
+    );
+    assert_eq!(parse_complex(",-0.0625"), None);
 }
