50d1dfa22235f440593e041511c2abfa7bee2c12
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2025-03-18 07:03:19 +0900

    Fix quickreplace

--- c02_quickreplace/src/main.rs
+++ c02_quickreplace/src/main.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 use std::env;
 use std::fs;
 use text_colorizer::*;
@@ -18,7 +19,15 @@ fn main() {
         }
     };
 
-    match fs::write(&args.output, &data) {
+    let replaced_data = match replace(&args.target, &args.replacement, &data) {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!("{} failed to replace {:?}", "Error:".red().bold(), e);
+            std::process::exit(1);
+        }
+    };
+
+    match fs::write(&args.output, &replaced_data) {
         Ok(_) => {}
         Err(e) => {
             eprintln!(
@@ -67,4 +76,9 @@ fn parse_args() -> Arguments {
         filename: args[2].clone(),
         output: args[3].clone(),
     }
+}
+
+fn replace(target: &str, replacement: &str, text: &str) -> Result<String, regex::Error> {
+    let re = Regex::new(&target)?;
+    Ok(re.replace_all(text, replacement).to_string())
 }
