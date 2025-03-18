218ca296b0b90672c7115e4017d007dd7e96e87a
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2025-03-19 08:19:39 +0900

    Type annotation

--- c03_example/src/main.rs
+++ c03_example/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let v1 = build_vector1();
+    let v2 = build_vector2();
+    println!("v1: {:?}", v1);
+    println!("v2: {:?}", v2);
+}
+
+fn build_vector1() -> Vec<i16> {
+    let mut v: Vec<i16> = Vec::<i16>::new();
+    v.push(10i16);
+    v.push(20i16);
+    v
+}
+
+fn build_vector2() -> Vec<i16> {
+    let mut v = Vec::new();
+    v.push(10);
+    v.push(20);
+    v
 }
