dde160ee537534b7048f603037ac4a41c8a6baea
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2025-04-09 09:29:35 +0900

    Transcribed 9.5

--- c09/example/src/main.rs
+++ c09/example/src/main.rs
@@ -43,6 +43,34 @@ fn main() {
     // 9.3 unit
     #[allow(unused_variables)]
     let o = OneSuch;
+
+    // 9.5 method define
+    let mut q = Queue {
+        older: Vec::new(),
+        younger: Vec::new(),
+    };
+
+    q.push('0');
+    q.push('1');
+    assert_eq!(q.pop(), Some('0'));
+    q.push('∞');
+    assert_eq!(q.pop(), Some('1'));
+    assert_eq!(q.pop(), Some('∞'));
+    assert_eq!(q.pop(), None);
+
+    assert!(q.is_empty());
+    q.push('Θ');
+    assert!(!q.is_empty());
+    q.pop();
+
+    q.push('P');
+    q.push('D');
+    assert_eq!(q.pop(), Some('P'));
+    q.push('X');
+    let (older, younger) = q.split();
+    // println!("q is {:?}", q); // value borrowed here after move
+    assert_eq!(older, vec!['D']);
+    assert_eq!(younger, vec!['X']);
 }
 #[derive(Debug)]
 struct GrayscaleMap {
@@ -91,3 +119,31 @@ struct Bounds(usize, usize);
 struct Ascii(Vec<u8>);
 
 struct OneSuch;
+pub struct Queue {
+    older: Vec<char>,
+    younger: Vec<char>,
+}
+
+impl Queue {
+    pub fn push(&mut self, c: char) {
+        self.younger.push(c);
+    }
+    pub fn pop(&mut self) -> Option<char> {
+        if self.older.is_empty() {
+            if self.younger.is_empty() {
+                return None;
+            }
+            use std::mem::swap;
+            swap(&mut self.older, &mut self.younger);
+            self.older.reverse();
+        }
+        self.older.pop()
+    }
+    pub fn is_empty(&self) -> bool {
+        self.older.is_empty() && self.younger.is_empty()
+    }
+
+    pub fn split(self) -> (Vec<char>, Vec<char>) {
+        (self.older, self.younger)
+    }
+}
