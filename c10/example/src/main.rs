e1fe1bdf50c858e847ba7f04ce61064890a8e88b
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2025-04-16 19:25:55 +0900

    Transcribed to 10.1

--- c10/example/src/main.rs
+++ c10/example/src/main.rs
@@ -1,3 +1,69 @@
+use std::cmp::Ordering::{self, *};
+use std::mem::size_of;
+
 fn main() {
-    println!("Hello, world!");
+    // 10.1
+    assert_eq!(compare(1, 2), Less);
+
+    assert_eq!(size_of::<Ordering>(), 1);
+    assert_eq!(size_of::<HttpStatus>(), 2);
+
+    assert_eq!(HttpStatus::Ok as i32, 200);
+
+    assert_eq!(http_status(200), Some(HttpStatus::Ok));
+    assert_eq!(http_status(500), None);
+
+    assert_eq!(TimeUnit::Seconds.singular(), "second");
+}
+
+fn compare(a: i32, b: i32) -> Ordering {
+    if a > b {
+        Greater
+    } else if a < b {
+        Less
+    } else {
+        Equal
+    }
+}
+#[derive(Debug, PartialEq)]
+enum HttpStatus {
+    Ok = 200,
+    BadRequest = 400,
+    NotFound = 404,
+}
+
+fn http_status(code: u32) -> Option<HttpStatus> {
+    match code {
+        200 => Some(HttpStatus::Ok),
+        400 => Some(HttpStatus::BadRequest),
+        404 => Some(HttpStatus::NotFound),
+        _ => None,
+    }
+}
+
+#[derive(Debug, PartialEq, Copy, Clone, Eq)]
+#[allow(dead_code)]
+enum TimeUnit {
+    Seconds,
+    Minutes,
+    Hours,
+    Days,
+    Months,
+    Years,
+}
+
+impl TimeUnit {
+    fn plural(self) -> &'static str {
+        match self {
+            TimeUnit::Seconds => "seconds",
+            TimeUnit::Minutes => "minutes",
+            TimeUnit::Hours => "hours",
+            TimeUnit::Days => "days",
+            TimeUnit::Months => "months",
+            TimeUnit::Years => "years",
+        }
+    }
+    fn singular(self) -> &'static str {
+        self.plural().trim_end_matches('s')
+    }
 }
