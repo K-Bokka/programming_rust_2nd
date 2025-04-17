3ca863f88e017795a789e22221ad20010b99d609
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2025-04-17 18:45:54 +0900

    Transcribed to 10.1.1

--- c10/example/src/main.rs
+++ c10/example/src/main.rs
@@ -14,6 +14,20 @@ fn main() {
     assert_eq!(http_status(500), None);
 
     assert_eq!(TimeUnit::Seconds.singular(), "second");
+
+    // 10.1.1
+    let four_score_and_seven_years_ago = RoughTime::InThePast(TimeUnit::Years, 4 * 20 + 7);
+    let three_hours_from_now = RoughTime::InTheFuture(TimeUnit::Hours, 3);
+    let just_now = RoughTime::JustNow;
+    println!("{:?}", four_score_and_seven_years_ago);
+    println!("{:?}", three_hours_from_now);
+    println!("{:?}", just_now);
+
+    let unit_sphere = Shape::Sphere {
+        center: Point3D::ORIGIN,
+        radius: 1.0,
+    };
+    println!("{:?}", unit_sphere);
 }
 
 fn compare(a: i32, b: i32) -> Ordering {
@@ -66,4 +80,38 @@ impl TimeUnit {
     fn singular(self) -> &'static str {
         self.plural().trim_end_matches('s')
     }
+}
+
+#[derive(Debug, PartialEq, Copy, Clone)]
+enum RoughTime {
+    InThePast(TimeUnit, u32),
+    JustNow,
+    InTheFuture(TimeUnit, u32),
+}
+#[allow(dead_code)]
+#[derive(Debug)]
+struct Point3D(f32, f32, f32);
+
+impl Point3D {
+    const ORIGIN: Point3D = Point3D(0.0, 0.0, 0.0);
+}
+#[allow(dead_code)]
+#[derive(Debug)]
+enum Shape {
+    Sphere {
+        center: Point3D,
+        radius: f32,
+    },
+    Cuboid {
+        corner_1: Point3D,
+        corner_2: Point3D,
+    },
+}
+
+#[allow(dead_code)]
+enum RelationShipStatus {
+    Single,
+    InARelationship,
+    ItsComplicated(Option<String>),
+    ItsExtremelyComplicated { car: String, cdr: String },
 }
