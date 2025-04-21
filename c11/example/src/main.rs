338406bd2b3e91567bf31c1bd91259ca740dbb6e
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2025-04-22 06:40:50 +0900

    Transcribed to 11.0

--- c11/example/src/main.rs
+++ c11/example/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Chanter 11 / trait & generics");
+
+    let mut local_file = std::fs::File::create("hello.txt")?;
+    say_hello(&mut local_file)?;
+
+    let mut bytes = vec![];
+    say_hello(&mut bytes)?;
+    assert_eq!(bytes, b"Hello, world!\n");
+
+    let min_i32 = min(1, 2);
+    assert_eq!(min_i32, 1);
+
+    Ok(())
+}
+
+fn say_hello(out: &mut dyn std::io::Write) -> std::io::Result<()> {
+    out.write_all(b"Hello, world!\n")?;
+    out.flush()
+}
+
+fn min<T: Ord>(a: T, b: T) -> T {
+    if a <= b { a } else { b }
 }
