b6f334eace784542f0b1095e700be4f46dd5e63f
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2025-05-15 20:50:51 +0900

    Transcribed to 14.3

--- c14/example/src/main.rs
+++ c14/example/src/main.rs
@@ -1,6 +1,7 @@
 mod s00_intro;
 mod s01_capture;
 mod s02_f_c_types;
+mod s03_c_performance;
 
 fn main() {
     println!("Chapter 14");
@@ -13,4 +14,7 @@ fn main() {
 
     println!();
     s02_f_c_types::run();
+    
+    println!();
+    s03_c_performance::run();
 }
