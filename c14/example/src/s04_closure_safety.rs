5da199a9ae4908d6d7e542508635ad985708bc03
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2025-05-16 11:09:24 +0900

    Transcribed to 14.4.3

--- c14/example/src/s04_closure_safety.rs
+++ c14/example/src/s04_closure_safety.rs
@@ -22,4 +22,22 @@ pub fn run() -> () {
     #[allow(unused_variables)]
     let f = || drop(my_str); // error[E0525]: expected a closure that implements the `Fn` trait, but this closure only implements `FnOnce`
     // call_twice(f);
+    
+    // 14.4.3 FnMut
+    let mut i = 0;
+    let incr = || {
+        i += 1;
+        println!("Ding! is is now: {}", i);
+    };
+
+    fn call_twice_mut<F>(mut closure: F)
+    where
+        F: FnMut(),
+    {
+        closure();
+        closure();
+    }
+    
+    call_twice_mut(incr);
+    assert_eq!(i, 2);
 }
