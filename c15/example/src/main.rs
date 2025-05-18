a3ff9afef11d2e7893c347e72bb7dd3b150f482d
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2025-05-18 14:29:26 +0900

    Start chapter 15

--- c15/example/src/main.rs
+++ c15/example/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
     println!("Chapter 15. Iterator");
+
+    let n = 10;
+    println!("triangle: 1 ~ {} : {}", n, triangle(n));
+    println!("triangle_fold: 1 ~ {} : {}", n, triangle_fold(n));
+}
+
+fn triangle(n: i32) -> i32 {
+    let mut sum = 0;
+    for i in 1..=n {
+        sum += i;
+    }
+    sum
+}
+
+fn triangle_fold(n: i32) -> i32 {
+    (1..=n).fold(0, |sum, i| sum + i)
 }
