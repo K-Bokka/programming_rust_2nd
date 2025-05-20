3ffb77a3f9148b3ce383eb2b8f3578f00a1ae11b
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2025-05-21 06:38:29 +0900

    Transcribed to 15.2.2

--- c15/example/src/s02_create_iterator.rs
+++ c15/example/src/s02_create_iterator.rs
@@ -1,3 +1,5 @@
+use std::vec::IntoIter;
+
 pub fn run() {
     println!("15.2 Create Iterator");
 
@@ -19,4 +21,33 @@ pub fn run() {
     assert_eq!(path_iter.next(), Some(OsStr::new("/")));
     assert_eq!(path_iter.next(), Some(OsStr::new("Users")));
     assert_eq!(path_iter.next(), Some(OsStr::new("ak-yama")));
+
+    // 15.2.2 IntoIterator
+    use std::collections::BTreeSet;
+    let mut favorites = BTreeSet::new();
+    favorites.insert("Lucy in the Sky with Diamonds".to_string());
+    favorites.insert("Liebesträume No. 3".to_string());
+
+    let mut it = favorites.into_iter();
+    assert_eq!(it.next(), Some("Liebesträume No. 3".to_string()));
+    assert_eq!(it.next(), Some("Lucy in the Sky with Diamonds".to_string()));
+    assert_eq!(it.next(), None);
+
+    // 消費されているので空っぽになっている
+    // assert!(favorites.is_empty()); // error[E0382]: borrow of moved value: `favorites`
+
+    use std::fmt::Debug;
+
+    fn dump<T, U>(t: T)
+    where
+        T: IntoIterator<Item = U>,
+        U: Debug,
+    {
+        for u in t {
+            println!("{:?}", u);
+        }
+    }
+
+    let v = vec![1, 2, 3];
+    dump(v);
 }
