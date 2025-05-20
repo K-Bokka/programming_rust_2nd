4b2f7ca3208c0cb1a8ba2d19c2c90189d599e80a
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2025-05-21 06:54:14 +0900

    Transcribed to 15.2.3

--- c15/example/src/s02_create_iterator.rs
+++ c15/example/src/s02_create_iterator.rs
@@ -1,5 +1,3 @@
-use std::vec::IntoIter;
-
 pub fn run() {
     println!("15.2 Create Iterator");
 
@@ -50,4 +48,37 @@ pub fn run() {
 
     let v = vec![1, 2, 3];
     dump(v);
+
+    // 15.2.3 from_fn, successors
+    use rand::random;
+    use std::iter::from_fn;
+    let lengths: Vec<f64> = from_fn(|| Some(random::<f64>() - random::<f64>().abs()))
+        .take(10)
+        .collect();
+    println!("{:?}", lengths);
+
+    use num::Complex;
+    use std::iter::successors;
+
+    #[allow(dead_code)]
+    fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
+        let zero = Complex { re: 0.0, im: 0.0 };
+        successors(Some(zero), |&z| Some(z * z + c))
+            .take(limit)
+            .enumerate()
+            .find(|&(_i, z)| z.norm() > 4.0)
+            .map(|(i, _z)| i)
+    }
+
+    fn fibonacci() -> impl Iterator<Item = usize> {
+        let mut state = (0, 1);
+        from_fn(move || {
+            state = (state.1, state.0 + state.1);
+            Some(state.0)
+        })
+    }
+    assert_eq!(
+        fibonacci().take(8).collect::<Vec<_>>(),
+        vec![1, 1, 2, 3, 5, 8, 13, 21]
+    );
 }
