9f501f5659b5ac36d562825dfa28058c2900f193
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2025-05-23 07:03:41 +0900

    Transcribed to 15.3.2

--- c15/example/src/s03_iterator_adapter.rs
+++ c15/example/src/s03_iterator_adapter.rs
@@ -32,4 +32,33 @@ pub fn run() {
         }
     }
     assert_eq!(v, vec!["ponies", "giraffes", "squid"]);
+
+    // 15.3.2 filter_map, flat_map
+    use std::str::FromStr;
+
+    let text = "1\nfrond .25   289\n3.1415 estuary\n";
+    for number in text
+        .split_whitespace()
+        .filter_map(|s| f64::from_str(s).ok())
+    {
+        println!("{:4.2}", number.sqrt());
+    }
+
+    let text = "1\nfrond .25   289\n3.1415 estuary\n";
+    text.split_whitespace()
+        .map(|w| f64::from_str(w))
+        .filter(|r| r.is_ok())
+        .map(|r| r.unwrap())
+        .for_each(|n| println!("{:4.2}", n.sqrt()));
+
+    use std::collections::HashMap;
+    let mut major_cities = HashMap::new();
+    major_cities.insert("Japan", vec!["Tokyo", "Osaka", "Kyoto"]);
+    major_cities.insert("France", vec!["Paris", "Lyon", "Marseille"]);
+    major_cities.insert("Germany", vec!["Berlin", "Hamburg", "Stuttgart"]);
+    let countries = ["Japan", "Germany"];
+
+    for &city in countries.iter().flat_map(|country| &major_cities[country]) {
+        println!("{}", city);
+    }
 }
