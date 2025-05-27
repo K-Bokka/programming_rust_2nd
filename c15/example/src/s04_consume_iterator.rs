ccfce0a152b889b8bf3f5d4f3a5f3ddae88612f8
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2025-05-28 07:40:28 +0900

    Transcribe 15.4.1

--- c15/example/src/s04_consume_iterator.rs
+++ c15/example/src/s04_consume_iterator.rs
@@ -1,3 +1,14 @@
 pub fn run() {
     println!("15.4 Consume Iterator");
+
+    // 15.4.1 count, sum, product
+    fn triangle(n: u64) -> u64 {
+        (1..=n).sum()
+    }
+    assert_eq!(triangle(10), 55);
+
+    fn factrial(n: u64) -> u64 {
+        (1..=n).product()
+    }
+    assert_eq!(factrial(10), 3628800);
 }
