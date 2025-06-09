28a2069efad669fca46a422097efb8701231f322
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2025-06-09 19:10:16 +0900

    Add 16.3

--- c16/example/src/main.rs
+++ c16/example/src/main.rs
@@ -1,8 +1,12 @@
 mod s02_vec;
+mod s03_vec_deque;
 
 fn main() {
     println!("Chapter 16 Collection");
-    
+
     println!();
     s02_vec::run();
+
+    println!();
+    s03_vec_deque::run();
 }
---
+++ c16/example/src/s03_vec_deque.rs
@@ -0,0 +1,3 @@
+pub fn run() {
+    println!("16.3 VecDeque");
+}
