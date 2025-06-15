4d06d82ea0271671d27f06ade921a6d0deb57551
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2025-06-15 09:54:52 +0900

    Transcribed to 17.1.3

--- c17/example/src/s01_unicode.rs
+++ c17/example/src/s01_unicode.rs
@@ -19,4 +19,18 @@ pub fn run() {
     let unicode = '§';
     let latin1 = char_to_latin1(unicode).unwrap();
     println!("{:02X}", latin1);
+
+    println!("\n17.1.2 UTF-8");
+    assert_eq!(
+        "うどん: udon".as_bytes(),
+        &[
+            0xe3, 0x81, 0x86, // う
+            0xe3, 0x81, 0xa9, // ど
+            0xe3, 0x82, 0x93, // ん
+            0x3a, 0x20, 0x75, 0x64, 0x6f, 0x6e, // : udon
+        ]
+    );
+
+    println!("\n17.1.3 Text orientation");
+    assert_eq!("שָׁלוֹם".chars().next(), Some('ש'));
 }
