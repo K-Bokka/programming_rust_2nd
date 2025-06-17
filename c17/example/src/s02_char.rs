1691ebb3b41853ca370aab4e2df1fc7e347968c1
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2025-06-17 18:12:40 +0900

    Transcribe 17.2.3

--- c17/example/src/s02_char.rs
+++ c17/example/src/s02_char.rs
@@ -16,4 +16,16 @@ pub fn run() {
     assert_eq!('F'.to_digit(16), Some(15));
     assert_eq!(std::char::from_digit(15, 16), Some('f'));
     assert!(char::is_digit('f', 16));
+
+    println!("\n17.2.3 upper lower");
+    let mut upper = 'ß'.to_uppercase();
+    assert_eq!(upper.next(), Some('S'));
+    assert_eq!(upper.next(), Some('S'));
+    assert_eq!(upper.next(), None);
+
+    let ch = 'İ';
+    let mut lower = ch.to_lowercase();
+    assert_eq!(lower.next(), Some('i'));
+    assert_eq!(lower.next(), Some('\u{307}'));
+    assert_eq!(lower.next(), None);
 }
