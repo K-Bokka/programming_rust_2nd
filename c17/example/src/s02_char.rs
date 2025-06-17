616471ad1e7c263db7bffc5e13604f0780f5ca13
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2025-06-17 18:06:19 +0900

    Transcribe 17.2.1

--- c17/example/src/s02_char.rs
+++ c17/example/src/s02_char.rs
@@ -2,4 +2,14 @@ pub fn run() {
     println!("17.2 char");
 
     assert_eq!("カニ".chars().next(), Some('カ'));
+    
+    println!("\n17.2.1 char category");
+    
+    assert!(32u8.is_ascii_whitespace());
+    assert!(b'9'.is_ascii_digit());
+    
+    let line_tab = '\u{000b}';
+    assert_eq!(line_tab.is_whitespace(), true);
+    assert_eq!(line_tab.is_ascii_whitespace(), false);
+    
 }
