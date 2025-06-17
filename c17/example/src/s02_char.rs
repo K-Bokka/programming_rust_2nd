7be5d4510972d0dcb0e5a9a7bc6f0795b0fe0adb
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2025-06-17 18:08:45 +0900

    Transcribe 17.2.2

--- c17/example/src/s02_char.rs
+++ c17/example/src/s02_char.rs
@@ -2,14 +2,18 @@ pub fn run() {
     println!("17.2 char");
 
     assert_eq!("カニ".chars().next(), Some('カ'));
-    
+
     println!("\n17.2.1 char category");
-    
+
     assert!(32u8.is_ascii_whitespace());
     assert!(b'9'.is_ascii_digit());
-    
+
     let line_tab = '\u{000b}';
     assert_eq!(line_tab.is_whitespace(), true);
     assert_eq!(line_tab.is_ascii_whitespace(), false);
-    
+
+    println!("\n17.2.2 numeric");
+    assert_eq!('F'.to_digit(16), Some(15));
+    assert_eq!(std::char::from_digit(15, 16), Some('f'));
+    assert!(char::is_digit('f', 16));
 }
