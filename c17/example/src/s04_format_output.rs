114c29b8ab5e9abc5b21a1088eb229e0979b7bba
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2025-07-04 18:18:58 +0900

    Transcribe to 17.4.8

--- c17/example/src/s04_format_output.rs
+++ c17/example/src/s04_format_output.rs
@@ -158,5 +158,37 @@ pub fn run() {
         ),
         "       hel"
     );
-    assert_eq!(format!("{:.*}", get_limit(), content), "hel")
+    assert_eq!(format!("{:.*}", get_limit(), content), "hel");
+
+    println!("\n17.4.8 user defined class formatting");
+
+    use std::fmt;
+    impl fmt::Display for Complex {
+        fn fmt(&self, dest: &mut fmt::Formatter<'_>) -> fmt::Result {
+            let (re, im) = (self.re, self.im);
+            if dest.alternate() {
+                let abs = f64::sqrt(re * re + im * im);
+                let angle = f64::atan2(im, re) / std::f64::consts::PI * 180.0;
+                write!(dest, "{} < {}°", abs, angle)
+            } else {
+                let im_sign = if self.im < 0.0 { "-" } else { "+" };
+                write!(dest, "{} {} {}i", self.re, im_sign, f64::abs(self.im))
+            }
+        }
+    }
+
+    let one_twenty = Complex {
+        re: -0.5,
+        im: 0.866,
+    };
+    assert_eq!(format!("{}", one_twenty), "-0.5 + 0.866i");
+    let two_forty = Complex {
+        re: 0.5,
+        im: -0.866,
+    };
+    assert_eq!(format!("{}", two_forty), "0.5 - 0.866i");
+
+    let ninety = Complex { re: 0.0, im: 2.0 };
+    assert_eq!(format!("{}", ninety), "0 + 2i");
+    assert_eq!(format!("{:#}", ninety), "2 < 90°");
 }
