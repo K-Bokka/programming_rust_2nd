05922cdca0af194ecd7285f386656f17fd10de65
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2025-07-07 18:07:04 +0900

    Transcribe 17.6.2

--- c17/example/src/s06_normalize.rs
+++ c17/example/src/s06_normalize.rs
@@ -1,5 +1,6 @@
 use std::collections::hash_map::DefaultHasher;
 use std::hash::{Hash, Hasher};
+use unicode_normalization::UnicodeNormalization;
 
 pub fn run() {
     println!("17.6 Normalize");
@@ -15,4 +16,11 @@ pub fn run() {
 
     assert_eq!(hash("th\u{e9}"), 0x53e2d0734eb1dff3);
     assert_eq!(hash("the\u{301}"), 0x90d837f0a0928144);
+
+    println!("\n17.6.1 Normalization Forms");
+
+    println!("\n17.6.2 unicode-normalization crate");
+    assert_eq!("Phở".nfd().collect::<String>(), "Pho\u{31b}\u{309}");
+    assert_eq!("Phở".nfc().collect::<String>(), "Ph\u{1edf}");
+    assert_eq!("① Diﬃculty".nfkc().collect::<String>(), "1 Difficulty");
 }
