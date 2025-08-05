68a52e9e5d540c0516615f6cfbf5cc0f48efe93e
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2025-08-06 08:00:34 +0900

    Start 20.1

--- c20/example/src/main.rs
+++ c20/example/src/main.rs
@@ -1,3 +1,8 @@
+mod s01_sync_to_async;
+
 fn main() {
     println!("Chapter 20 async programming");
+
+    println!();
+    s01_sync_to_async::run().unwrap();
 }
---
+++ c20/example/src/s01_sync_to_async.rs
@@ -0,0 +1,5 @@
+pub fn run() -> Result<(), Box<dyn std::error::Error>> {
+    println!("20.1 Sync programming to Async programming");
+
+    Ok(())
+}
