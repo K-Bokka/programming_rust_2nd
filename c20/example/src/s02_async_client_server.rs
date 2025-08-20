b079d5519af396ab52c7afee71f8dc4159a39377
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2025-08-20 18:28:05 +0900

    Read to 20.2.10

--- c20/example/src/s02_async_client_server.rs
+++ c20/example/src/s02_async_client_server.rs
@@ -9,5 +9,17 @@ pub fn run() -> Result<(), Box<dyn std::error::Error>> {
 
     println!("20.2.4 Send packet");
 
+    println!("20.2.5 Receive packet");
+
+    println!("20.2.6 Client main function");
+
+    println!("20.2.7 Server main function");
+
+    println!("20.2.8 Chat connection management");
+
+    println!("20.2.9 Group table");
+
+    println!("20.2.10 Chat group");
+
     Ok(())
 }
