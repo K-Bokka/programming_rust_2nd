d8041587484890c0204bc77d30cd8d4c275d8c49
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2025-09-08 06:31:21 +0900

    Read 22.1

---
+++ c22/example/src/s01_what_unsafe.rs
@@ -0,0 +1,11 @@
+pub fn run() -> Result<(), Box<dyn std::error::Error>> {
+    println!("22.1 What is unsafe about it?");
+
+    // let mut a: usize = 0;
+    // let ptr = &mut a as *mut usize;
+    // unsafe {
+    //     *ptr.offset(3) = 0x7ffff72f484c;
+    // }
+
+    Ok(())
+}
