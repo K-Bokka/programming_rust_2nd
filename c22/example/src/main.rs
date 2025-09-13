734d7e00bf43307593ae0a829eda07a04a1c0394
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2025-09-13 12:09:42 +0900

    Read 22.6, but didn't build

--- c22/example/src/main.rs
+++ c22/example/src/main.rs
@@ -2,6 +2,7 @@ mod s01_what_unsafe;
 mod s02_unsafe_block;
 mod s03_ascii_mod;
 mod s04_unsafe_fn;
+mod s06_undefined_behavior;
 
 fn main() {
     println!("Chapter 22. unsafe code");
@@ -19,4 +20,7 @@ fn main() {
     s04_unsafe_fn::run().unwrap();
 
     println!("\n22.5 unsafe block or function");
+
+    println!();
+    s06_undefined_behavior::run().unwrap();
 }
